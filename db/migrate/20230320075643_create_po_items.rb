f022f83a7b31746851bad0b1077d5065fb6b5d02
alex rigney <arign020@gmail.com>
2023-03-24 21:15:22 -0400

    rebase and add limits to db

--- db/migrate/20230320075643_create_po_items.rb
+++ db/migrate/20230320075643_create_po_items.rb
@@ -1,10 +1,10 @@
 class CreatePoItems < ActiveRecord::Migration[7.0]
   def change
     create_table :po_items do |t|
-      t.string :bid, null: false
-      t.integer :quantity, null: false
-      t.string :colour, null: false
-      t.string :size, null: false
+      t.string :bid, null: false, limit: 20
+      t.integer :price, null: false, precision: 10, scale: 2
+      t.references :purchase_order
+      t.references :item
       
       t.timestamps
     end
