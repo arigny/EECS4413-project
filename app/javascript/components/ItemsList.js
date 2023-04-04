4a6fdbbdb474ba0d3c86b529013baf1efd5e9c88
alex rigney <arign020@gmail.com>
2023-04-04 16:42:26 -0400

    style catalog views

--- app/javascript/components/ItemsList.js
+++ app/javascript/components/ItemsList.js
@@ -2,22 +2,20 @@ import React from 'react';
 
 const ItemsList = ({ items }) => {
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Bid</th>
-                </tr>
-            </thead>
-            <tbody>
-                {items.map((item) => (
-                    <tr key={item.id}>
-                        <td>{item.name}</td>
-                        <td>{item.bid}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
+        <div className='card-grid'>
+        {items.map((item) => (
+            <div key={item.id} className="card hoverable" style={{width: '18rem'}}>
+                <div className="card-body">
+                    <h5 className="card-title">{item.name}</h5>
+                    <h6 className="card-subtitle mb-2 text-muted">${item.price}</h6>
+                    <p className="card-text">{item.description}</p>
+                    <a href={`po_items/new`} className="btn btn-primary">Add to cart</a>
+                    <a href={`items/${item.id}`} className="card-link"> See details</a>
+                </div>
+            </div>
+        ))}
+        <br/>
+        </div>
     );
 };
 
