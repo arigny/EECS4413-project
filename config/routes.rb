6c189f2c3d019a5db84f8de40cf872199ee5c080
alex rigney <arign020@gmail.com>
2023-04-09 05:36:40 -0400

    login/logout and track user session

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   resources :purchase_orders
   resources :po_items
   resources :items
+  delete 'logout' => 'sessions#destroy'
+  get 'login' => 'sessions#new', as: :login
+  post 'login' => 'sessions#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
