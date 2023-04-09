d2db5b896d138118ece68173838e9604fc93c188
chinyee8 <laichinyee1101@gmail.com>
2023-04-08 20:25:41 -0400

    add chatbot

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,15 +1,15 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createRoot } from 'react-dom/client';
-import ReactRailsUJS from 'react_ujs';
+import "@hotwired/turbo-rails";
+import "./controllers";
+import React from "react";
+import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
+import ReactRailsUJS from "react_ujs";
 import HelloWorld from "./components/HelloWorld";
 import ItemsList from "./components/ItemsList";
 
-ReactRailsUJS.registerComponent('HelloWorld', HelloWorld);
-ReactRailsUJS.registerComponent('ItemsList', ItemsList);
+ReactRailsUJS.registerComponent("HelloWorld", HelloWorld);
+ReactRailsUJS.registerComponent("ItemsList", ItemsList);
 
 global.React = React;
 global.ReactDOM = ReactDOM;
