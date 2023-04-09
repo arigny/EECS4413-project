5d22a5ff5c004fdab92b360a203205ae8c1b7724
alex rigney <arign020@gmail.com>
2023-04-09 02:52:21 -0400

    implement add-to-cart button for items

--- app/javascript/components/ItemsList.js
+++ app/javascript/components/ItemsList.js
@@ -1,6 +1,34 @@
 import React from 'react';
 
-const ItemsList = ({ items }) => {
+const ItemsList = ({ items, csrfToken, purchaseOrderId }) => {
+    const addToCart = (item, csrfToken) => {
+        fetch("/po_items", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "X-CSRF-Token": csrfToken
+            },
+            body: JSON.stringify({ item_id: item.id, bid: item.bid, price: item.price, purchase_order_id: purchaseOrderId})
+        })
+        .then(response => {
+            if (response.ok) {
+                alert("Item added to cart successfully!");
+            } else {
+                alert("Failed to add item to cart.");
+            }
+            
+            const contentType = response.headers.get("content-type");
+            if (contentType && contentType.indexOf("application/json") !== -1) {
+                return response.json();
+            } else {
+                // console.error("Response doesn't have JSON content type");
+                // throw new Error("Invalid JSON response");
+            }
+        })
+        .then(data => console.log("Response data: ", data))
+        .catch(error => console.error("Error in adding item to cart: ", error));
+    };
+
     return (
         <div className='card-grid'>
         {items.map((item) => (
@@ -9,7 +37,8 @@ const ItemsList = ({ items }) => {
                     <h5 className="card-title">{item.name}</h5>
                     <h6 className="card-subtitle mb-2 text-muted">${item.price}</h6>
                     <p className="card-text">{item.description}</p>
-                    <a href={`po_items/new`} className="btn btn-primary">Add to cart</a>
+                    <button onClick={() => addToCart(item, csrfToken)} className="btn btn-primary">Add to Cart</button>
+                    {/* <a href={`po_items/new`} className="btn btn-primary">Add to cart</a> */}
                     <a href={`items/${item.id}`} className="card-link"> See details</a>
                 </div>
             </div>
