c3ee9075b98b21990a80f3b7a5746aacc115db0f
alex rigney <arign020@gmail.com>
2023-04-09 22:20:50 -0400

    allow credit_card_number to be nil

--- app/models/purchase_order.rb
+++ app/models/purchase_order.rb
@@ -1,23 +1,25 @@
 class PurchaseOrder < ApplicationRecord
     belongs_to :address
     has_many :po_items, dependent: :destroy
+    after_create :destroy_po_items, if: -> { status == "ORDERED" }
 
     attr_accessor :credit_card_number
-    validates :credit_card_number, presence: true
-    validate :mock_credit_card_authentication
-    after_create :destroy_po_items, if: -> { status == "ORDERED" }
+    validates :credit_card_number, presence: true, unless: :rails_console_is_seeding?
+    validate :mock_credit_card_authentication, unless: :rails_console_is_seeding?
 
     validates :lname, :fname, :status, :address_id, presence: true
 
     private
 
     def mock_credit_card_authentication
+        return errors.add(:credit_card_number, "is not present") if credit_card_number.nil?
+    
         credit_card_digits = credit_card_number.gsub(/\D/, "")
         return errors.add(:credit_card_number, "is invalid") if credit_card_digits.length != 16
-
+    
         most_recent_orders = PurchaseOrder.order("created_at DESC").limit(3).pluck(:status)
         denied_check = most_recent_orders.count("ORDERED") == 2
-
+    
         if denied_check
             # errors.add(:credit_card_number, "transaction failed")
             self.status = "DENIED"
@@ -28,5 +30,9 @@ class PurchaseOrder < ApplicationRecord
 
     def destroy_po_items
         po_items.destroy_all
+    end
+        
+    def rails_console_is_seeding?
+        Rails.const_defined?('Console') || (File.basename($0) == 'seed')
     end
 end
